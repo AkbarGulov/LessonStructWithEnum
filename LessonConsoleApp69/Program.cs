4b2f9849ecedb37313d63bb6d85139b800bda3dc
AkbarGM <56382572+AkbarGM@users.noreply.github.com>
2020-09-26 14:35:47 +0500

    Add project files.

---
+++ LessonConsoleApp69/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonConsoleApp69
+{
+    class Program
+    {
+        enum Gender
+        {
+            Man,
+            Woman
+        }
+        struct Student
+        {
+            public string Surname;
+            public string Name;
+            public int Age;
+            public Gender Gender;
+        }
+        static void Main(string[] args)
+        {
+            Student parviz = new Student();
+
+            parviz.Name = "Parviz";
+            parviz.Surname = "Azizov";
+            parviz.Age = 23;
+            parviz.Gender = Gender.Man;
+
+
+            Console.WriteLine($"Name: {parviz.Name}\n" +
+                $"Surname: {parviz.Surname}\n"+
+                $"Age: {parviz.Age}\n" +
+                $"Gender: {parviz.Gender}\n");
+
+            Console.ReadLine();
+        }
+    }
+}
